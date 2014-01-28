74f3e6e698e9bae6073e80970d89b2245f657360
Andy Christianson <achristianson@asuslaptop>
2014-01-28 17:18:51 -0500

    added image example

--- project.js
+++ project.js
@@ -1,31 +1,25 @@
 var sketchProc = function(processing) {
-	
-    // put variables here, just like kahn academy
-    var exampleVar = 10;
+
+    var img = processing.loadImage("photo.jpg");
 
     // put functions here, just like kahn academy
     var exampleFunction = function() {
-	processing.rect(30, 20, 55, 55);
+        processing.rect(30, 20, 55, 55);
     };
 
     // this code is executed once when the program is started
     processing.setup = function() {
 
-	// set up the size of the canvas (you probably don't want to change this!)
-	processing.size(800, 600);
+        // set up the size of the canvas (you probably don't want to change this!)
+        processing.size(800, 600);
 
-	// some drawing commands; feel free to change these
-	processing.fill(255, 0, 0);
-
-	// draw some text
-	processing.text("Hello, World!", exampleVar, exampleVar);
-
-	// call a function (defined up above)
-	exampleFunction();
     };
 
     // override draw function, by default it will be called 60 times per second
     processing.draw = function() {
+
+        // draw some text
+        processing.image(img, 200, 30, 100, 100);
 
     };
 }
